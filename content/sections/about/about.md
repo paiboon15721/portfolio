bbdc4e62ed3f597baa308b1753d1f95e6f799b3f
paiboon <paiboon.a@thoughtworks.com>
2025-01-31 17:17:53 +0700

    initial portfolio 2025

---
+++ content/sections/about/about.md
@@ -0,0 +1,10 @@
+---
+imageSrc: "../../images/me.jpg"
+imageAlt: "About me"
+---
+
+Start the journey as a software developer more than ten years ago. I've collaborated with talented people to create digital products for both business and consumer use.
+
+I'm a fast learner and interested in the entire technology spectrum. I saw the technologies changing over time that makes me more excited because we as a humanity have betters way to solve the problems.
+
+So, I define myself by the work I want to do, not what I've done. I believe in long-life learning, continue challenging myself, and do interesting things that matter.
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Paiboon's Portfolio" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="interests" heading="Interests & Skills" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="contact" heading="Contact Me" />
+      </Page>
+    </>
+  );
+}
